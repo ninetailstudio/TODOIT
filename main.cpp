fbc08b583515a887a38ef71d047b19ea1d1c17e9
Jordan Powell <jordanpowell487@gmail.com>
2023-10-08 09:48:31 -0400

    Removed Executable from Repo, and add user validation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <limits>
+#include <ios>
 #include <vector>
+
 
 void displayMenu() {
    std::cout << "----- To Do List Menu -----" << std::endl;
@@ -16,11 +19,17 @@ void addTask(std::vector<std::string> &tasks, std::string task) {
 }
 
 void deleteTask(std::vector<std::string> &tasks, int taskNumber) {
-      tasks.erase(tasks.begin() + taskNumber);
+    if(taskNumber < tasks.size()) 
+        tasks.erase(tasks.begin() + taskNumber);
+    else
+        std::cout << "Invalid request" << std::endl;
 }
 
 void completeTask(std::vector<std::string> &tasks, int taskNumber) {
-      tasks[taskNumber] += " COMPLETE";
+    if(taskNumber < tasks.size()) 
+        tasks[taskNumber] += " COMPLETE";
+    else
+        std::cout << "Invalid request" << std::endl;
 }
 
 void getTasks(std::vector<std::string> tasks) {
@@ -38,7 +47,14 @@ void getTasks(std::vector<std::string> tasks) {
 }
 
 
-int main() {
+int main(int argc, char* argv[]) {
+
+
+    if(argc > 1) {
+
+    }
+
+    
 
     std::vector<std::string> tasks;
     int selector = 0; 
@@ -46,12 +62,26 @@ int main() {
     displayMenu();
 
     while(selector != 5) {
+
         std::string task;
         int taskSelector = 0;
 
         std::cout << "Enter a number" << std::endl;
         std::cin >> selector;
         std::cin.ignore();
+
+
+        if(std::cin.fail()){
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
+        }
+
+
+        if(selector > 5 || selector < 1) {
+            std::cout << "Not a valid input" << std::endl;
+            selector = 4;
+        }
+
 
         switch(selector) {
             case(1):
@@ -80,6 +110,7 @@ int main() {
             default:
                 break;
         }
+
     }
 
 
